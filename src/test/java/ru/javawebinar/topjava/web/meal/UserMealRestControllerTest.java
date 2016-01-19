4ce4e531b20259fdc1d31b851b884b7207cf9dc5
savimar <sam409@yandex.ru>
2016-01-19 14:55:54 +0300

    add rest controller

---
+++ src/test/java/ru/javawebinar/topjava/web/meal/UserMealRestControllerTest.java
@@ -0,0 +1,41 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by B on 19.01.2016.
+ */
+public class UserMealRestControllerTest {
+
+    @Test
+    public void testGet() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+
+    }
+
+    @Test
+    public void testGetBetween() throws Exception {
+
+    }
+}
